727d933746c76659b8afdaf68d842190d2afa9ef
Maksim <42644986+msmaxx@users.noreply.github.com>
2021-05-19 15:50:15 +0300

    Update pageHeading.js

--- src/components/currencies/pageHeading.js
+++ src/components/currencies/pageHeading.js
@@ -19,7 +19,19 @@ const openInNewTab = (url) => {
     if (newWindow) newWindow.opener = null
 }
 
-export default function CurrencyPageHeading({name, contract, symbol, logo, chat, twitter, website, descrption, id, upVotes, downVotes}) {
+export default function CurrencyPageHeading({
+                                                name,
+                                                contract,
+                                                symbol,
+                                                logo,
+                                                chat,
+                                                twitter,
+                                                website,
+                                                descrption,
+                                                id,
+                                                upVotes,
+                                                downVotes
+                                            }) {
     return (
         <div>
             <div className="lg:flex lg:items-center lg:justify-between">
@@ -42,8 +54,8 @@ export default function CurrencyPageHeading({name, contract, symbol, logo, chat,
                                 <GetPrice contractAddress={contract}/>
                             </h2>
                             <div className="hidden sm:block ml-3 flex-1 mx-5">
-                            <Get24HChange contractAddress={contract}/>
-                        </div>
+                                <Get24HChange contractAddress={contract}/>
+                            </div>
                         </div>
                     </div>
 
@@ -58,82 +70,78 @@ export default function CurrencyPageHeading({name, contract, symbol, logo, chat,
                         </div>
                     </div>
 
-
                     <div className="flex-wrap">
                         <GetVotingButtons symbol={symbol} id={id} upVotes={upVotes} downVotes={downVotes}/>
                     </div>
-
                 </div>
             </div>
 
 
-            <div className="container flex flex-wrap mx-auto items-center">
+            <div className="flex-wrap">
 
-                <div className="flex-wrap">
+                <button className="flex-wrap my-2 bg-purple-100 text-purple-600 p-2 rounded flex items-center"
+                        onClick={() => {
+                            navigator.clipboard.writeText(contract)
+                        }}> Contract:
+                    <span className="mx-2 bg-purple-200 p-1 rounded text-purple-500 text-xs ml-2">{contract}</span>
+                    <BiCopy/>
+                </button>
 
-                    <button className="flex-wrap my-2 bg-purple-100 text-purple-600 p-2 rounded leading-none flex items-center"
-                            onClick={() => {
-                                navigator.clipboard.writeText(contract)
-                            }}>
-                        Contract Addres:
-                        <span className="mx-2 bg-purple-200 p-1 rounded text-purple-500 text-xs ml-2">{contract}</span>
-                        <BiCopy/>
-                    </button>
+            </div>
 
-
-
-                    <div className="flex-nowrap">
-                        <button
-                            className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4 rounded-l"
-                            onClick={() => openInNewTab(website)}>
-                            <HiExternalLink/>
-                            <span className="mx-2">
+                <div className="flex-nowrap">
+                    <button
+                        className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4 rounded-l"
+                        onClick={() => openInNewTab(website)}>
+                        <HiExternalLink/>
+                        <span className="mx-2">
                         Website
                     </span>
-                        </button>
-                        <button
-                            className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4"
-                            onClick={() => openInNewTab(twitter)}>
-                            <FiTwitter/>
-                            <span className="mx-2">Twitter</span>
-                        </button>
-                        <button
-                            className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4"
-                            onClick={() => openInNewTab(chat)}>
-                            <HiOutlineChatAlt/>
-                            <span className="mx-2">Chat</span>
-                        </button>
+                    </button>
+                    <button
+                        className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4"
+                        onClick={() => openInNewTab(twitter)}>
+                        <FiTwitter/>
+                        <span className="mx-2">Twitter</span>
+                    </button>
+                    <button
+                        className="inline-flex items-center bg-purple-200 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4"
+                        onClick={() => openInNewTab(chat)}>
+                        <HiOutlineChatAlt/>
+                        <span className="mx-2">Chat</span>
+                    </button>
 
-                        <button className="inline-flex items-center bg-purple-200
+                    <button className="inline-flex items-center bg-purple-200
                 hover:bg-purple-300 text-purple-700 font-medium py-2 px-4 rounded-r"
-                                onClick={() => openInNewTab(`https://bscscan.com/token/${contract}`)}>
-                            <BiScan/>
-                            <span className="mx-2">BscScan</span>
-                        </button>
-                    </div>
+                            onClick={() => openInNewTab(`https://bscscan.com/token/${contract}`)}>
+                        <BiScan/>
+                        <span className="mx-2">BscScan</span>
+                    </button>
+                </div>
 
 
-                    <div className="my-4">
-                        <button className="inline-flex items-center bg-yellow-100
+                <div className="flex-nowrap my-2">
+                    <button className="inline-flex items-center bg-yellow-100
                 hover:bg-yellow-200 text-yellow-800 font-medium py-2 px-4 rounded-l"
-                                onClick={() => openInNewTab(`https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=${contract}`)}>
-                            <GiTwoCoins/>
-                            <span className="mx-2">Buy on PCS (V1)</span>
-                        </button>
+                            onClick={() => openInNewTab(`https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=${contract}`)}>
+                        <GiTwoCoins/>
+                        <span className="mx-2">Buy on PCS (V1)</span>
+                    </button>
 
+                    <div>
                         <button className="inline-flex items-center bg-yellow-100
                 hover:bg-yellow-200 text-yellow-800 font-medium py-2 px-4 rounded-r"
                                 onClick={() => openInNewTab(`https://exchange.pancakeswap.finance/#/swap?inputCurrency=${contract}`)}>
                             <GiTwoCoins/>
                             <span className="mx-2">Buy on PCS (V2)</span>
                         </button>
-
-                        <div className="my-6 w-full">
-                            <h1 className="my-4 sm:text-3xl text-2xl font-medium title-font  text-gray-900">About {name}</h1>
-                            <div className="h-1 w-20 bg-purple-200 rounded"/>
-                        </div>
-                        <p className="my-4 w-full leading-relaxed text-gray-500">{descrption}</p>
                     </div>
+
+                    <div className="my-6 w-full">
+                        <h1 className="my-4 sm:text-3xl text-2xl font-medium title-font  text-gray-900">About {name}</h1>
+                        <div className="h-1 w-20 bg-purple-200 rounded"/>
+                    </div>
+                    <p className="my-4 w-full leading-relaxed text-gray-500">{descrption}</p>
                 </div>
 
                 <div className="flex-auto mx-4">
@@ -145,16 +153,19 @@ export default function CurrencyPageHeading({name, contract, symbol, logo, chat,
                             <dl>
                                 <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-bold text-gray-500">{symbol} price</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 font-bold sm:mt-0 sm:col-span-2"><GetPrice contractAddress={contract}/></dd>
+                                    <dd className="mt-1 text-sm text-gray-900 font-bold sm:mt-0 sm:col-span-2"><GetPrice
+                                        contractAddress={contract}/></dd>
                                 </div>
                                 <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-medium text-gray-500">Transactions (24H)</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"><GetTxns24h contractAddress={contract}/>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"><GetTxns24h
+                                        contractAddress={contract}/>
                                     </dd>
                                 </div>
                                 <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-medium text-gray-500">Liquidity</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"><GetLiquidityUSD contractAddress={contract}/></dd>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"><GetLiquidityUSD
+                                        contractAddress={contract}/></dd>
                                 </div>
                                 {/*<div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt className="text-sm font-medium text-gray-500">Market Cap</dt>
@@ -162,7 +173,6 @@ export default function CurrencyPageHeading({name, contract, symbol, logo, chat,
                                 </div>*/}
                             </dl>
                         </div>
-                    </div>
                 </div>
             </div>
         </div>

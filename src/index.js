e1d0ebf99e4d56b8d2c6e20ca037cde00935abc8
Maksim <42644986+msmaxx@users.noreply.github.com>
2021-05-20 21:04:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import nightwind from "nightwind/helper"
 
 
 import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />
+            <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );
